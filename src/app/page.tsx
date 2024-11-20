62ab1d4c7053d33c3db14fec4e502e58ed4c773b
David Marom <davidmarom@hotmail.com>
2024-11-20 12:07:05 +0200

    slider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,23 @@
 'use client';
-
+import { useState } from 'react';
 import { Card } from '@/app/components';
 
 export default function Home() {
+  const [padding, setPadding] = useState(30);
+
   return (
     <div>
+      <input
+        type="range"
+        min="0"
+        max="100"
+        value={padding}
+        onChange={(e) => setPadding(parseInt(e.target.value))}
+      />
+      
       <Card
-        $primary={false}
-        $padding='30px'
+        $primary={true}
+        $padding={`${padding}px`}
       >
         Primary Card
       </Card>
